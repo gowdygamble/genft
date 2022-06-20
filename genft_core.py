80e91b7cccf22b2a671d6d971ffdc9e9d53b4947
Paul <paulgamble49@gmail.com>
2022-06-20 16:02:14 -0700

    config error checking

--- genft_core.py
+++ genft_core.py
@@ -18,6 +18,79 @@ def norm_all_properties(property_dict):
     for k in property_dict:
         norm_property_distribution(property_dict[k])
 
+def check_config_against_directory(config_fn):
+    with open(config_fn, 'r') as stream:
+        try:
+            parsed_yaml = yaml.safe_load(stream)
+            #print(parsed_yaml)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    property_folders = glob.glob(os.path.join(parsed_yaml["asset_directory"], "*"))
+    property_folder_names = [os.path.basename(x) for x in property_folders]
+
+    config_property_names = list(parsed_yaml["properties"])
+    #print(config_property_names)
+
+    errors = []
+
+    print("1/4 Checking config properties against folders")
+    for cfg_prop_name in config_property_names:
+        if cfg_prop_name not in property_folder_names:
+            #print("Config property missing from asset dir: ", cfg_prop_name)
+            errors.append("Config property missing from asset dir: " + cfg_prop_name)
+
+
+    print("2/4 Checking folders against config properties")
+    for property_folder in property_folders:
+        property_name = os.path.basename(property_folder)
+        if property_name not in config_property_names:
+            #print("Property folder not found in config: ", property_name)
+            errors.append("Property folder not found in config: " + property_name)
+
+    print("3/4 Checking property asset values against asset files")
+    for property_name, property_folder in zip(property_folder_names, property_folders):
+        if property_name in config_property_names:
+            err = check_property_against_assets(property_name, parsed_yaml["properties"][property_name], property_folder)
+            if err:
+                errors.append(err)
+        else:
+            err = "Warning: " + property_name + " not in config, skipping asset check."
+            print(err)
+            errors.append(err)
+
+    print("4/4 Checking property distribution normalization")
+    print("NOT IMPLEMENTED")
+
+    print("")
+    print("--------------")
+    print("")
+    print("Errors:")
+    for err in errors:
+        print(err)
+    print("")
+    print("--------------")
+    print("")
+    print("Done checking config against directory")
+
+
+def check_property_against_assets(property_name, property_distribution_dict, property_folder):
+    property_asset_files = glob.glob(os.path.join(property_folder, "*"))
+    property_asset_names =  [os.path.basename(x) for x in property_asset_files]
+    property_asset_names =  [x.split(".")[0] for x in property_asset_names]
+
+    config_prop_names = list(property_distribution_dict.keys())
+    #print(property_name, property_asset_names, config_prop_names)
+    if set(config_prop_names) == set(property_asset_names):
+        return False
+    else:
+        return ("Asset file / config property value mismatch: " + property_name
+        + ", config: " + str(config_prop_names) + ", asset files: "
+        + str(property_asset_names))
+
+
+
+
 def generate_config_from_directory(directory_name, project_name):
     property_folders = glob.glob(os.path.join(directory_name, "*"))
     property_dict = {}
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-from genft_core import generate_config_from_directory
+from genft_core import generate_config_from_directory, check_config_against_directory
 
 d = "art/snek_1"
+config_fn = "snek_2_config.yml"
 
-generate_config_from_directory(d, "snek_1")
+#generate_config_from_directory(d, "snek_1")
+check_config_against_directory(config_fn)
